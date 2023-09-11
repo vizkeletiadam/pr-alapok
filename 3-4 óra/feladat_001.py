e82238982f5950725c7b153ac1d2a743bf621481
vizkeletiadam <113334882+vizkeletiadam@users.noreply.github.com>
2023-09-11 14:05:55 +0200

    Add files via upload

---
+++ 3-4 óra/feladat_001.py
@@ -0,0 +1,15 @@
+# feladat_001
+""""
+kérjük be a billentyűzetről a nevünket és
+irassuk ki a képernyőre!
+"""""
+"""nev = input('kérek egy nevet: ')
+print("a megadott név a következő: {nev}")
+"""
+
+vnev = input("kérek egy vezetéknevet: ")
+knev = input("kérek egy keresztnevet: ")
+print("a megadott név a következő: {vnev} {knev}")
+print(f"a megadott név a következő: {vnev} {knev}")
+print(f"a vnev változó tipusa: {type(vnev)}")
+print(f"a knev változó tipusa: {type(knev)}")
---
+++ 3-4 óra/feladat_002.py
@@ -0,0 +1,20 @@
+# feladat_002
+""""
+kérjük be a billentyűzetről a nevünket és
+irassuk ki a két szám összegét a képernyőre!
+"""""
+
+szam_1 = input('kérek egy számot: ')
+szam_1 = int(szam_1)
+szam_3 = int
+
+szam_2 = int(input("kérek egy másik számot: "))
+osszeg = szam_1 + szam_2
+
+print(f"a megadott két szám összege: {szam_1 + szam_2}")
+print(f"a megadott két szám összege: {osszeg}")
+print(f"a szam_1 változó tipusa: {type(szam_1)}")
+print(f"a szam_2 változó tipusa: {type(szam_2)}")
+print(f"az összeg változó tipusa: {type(osszeg)}")
+print(f"a szam_3 változó értéke: {szam_3}")
+print(print(f"az összeg változó tipusa: {type(szam_3)}"))
